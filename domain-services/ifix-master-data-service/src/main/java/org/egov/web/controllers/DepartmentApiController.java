1fcc6eaac12abc092753743079385d8645e05008
pintu-eGov <pintu.kumar@egovernments.org>
2021-08-09 17:05:44 +0530

    Department search Api

--- domain-services/ifix-master-data-service/src/main/java/org/egov/web/controllers/DepartmentApiController.java
+++ domain-services/ifix-master-data-service/src/main/java/org/egov/web/controllers/DepartmentApiController.java
@@ -3,9 +3,10 @@ package org.egov.web.controllers;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.swagger.annotations.ApiParam;
-import org.egov.web.models.DepartmentRequest;
-import org.egov.web.models.DepartmentResponse;
-import org.egov.web.models.DepartmentSearchRequest;
+import org.egov.common.contract.response.ResponseHeader;
+import org.egov.service.DepartmentService;
+import org.egov.util.ResponseHeaderCreator;
+import org.egov.web.models.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,11 +18,12 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 import java.io.IOException;
+import java.util.List;
 
 @javax.annotation.Generated(value = "org.egov.codegen.SpringBootCodegen", date = "2021-08-02T16:24:12.742+05:30")
 
 @Controller
-@RequestMapping("/eGovTrial/iFIX-Master-Data/1.0.0")
+@RequestMapping("/department/v1")
 public class DepartmentApiController {
 
     private final ObjectMapper objectMapper;
@@ -29,37 +31,30 @@ public class DepartmentApiController {
     private final HttpServletRequest request;
 
     @Autowired
+    private DepartmentService departmentService;
+
+    @Autowired
+    private ResponseHeaderCreator responseHeaderCreator;
+
+    @Autowired
     public DepartmentApiController(ObjectMapper objectMapper, HttpServletRequest request) {
         this.objectMapper = objectMapper;
         this.request = request;
     }
 
-    @RequestMapping(value = "/department/v1/_create", method = RequestMethod.POST)
+    @RequestMapping(value = "/_create", method = RequestMethod.POST)
     public ResponseEntity<DepartmentResponse> departmentV1CreatePost(@ApiParam(value = "Details for the new department + RequestHeader (meta data of the API).", required = true) @Valid @RequestBody DepartmentRequest body) {
-        String accept = request.getHeader("Accept");
-        if (accept != null && accept.contains("")) {
-            try {
-                return new ResponseEntity<DepartmentResponse>(objectMapper.readValue("", DepartmentResponse.class), HttpStatus.NOT_IMPLEMENTED);
-            } catch (IOException e) {
-                return new ResponseEntity<DepartmentResponse>(HttpStatus.INTERNAL_SERVER_ERROR);
-            }
-        }
 
         return new ResponseEntity<DepartmentResponse>(HttpStatus.NOT_IMPLEMENTED);
     }
 
-    @RequestMapping(value = "/department/v1/_search", method = RequestMethod.POST)
+    @RequestMapping(value = "/_search", method = RequestMethod.POST)
     public ResponseEntity<DepartmentResponse> departmentV1SearchPost(@ApiParam(value = "Details for the department search criteria + RequestHeader (meta data of the API).", required = true) @Valid @RequestBody DepartmentSearchRequest body) {
-        String accept = request.getHeader("Accept");
-        if (accept != null && accept.contains("")) {
-            try {
-                return new ResponseEntity<DepartmentResponse>(objectMapper.readValue("", DepartmentResponse.class), HttpStatus.NOT_IMPLEMENTED);
-            } catch (IOException e) {
-                return new ResponseEntity<DepartmentResponse>(HttpStatus.INTERNAL_SERVER_ERROR);
-            }
-        }
-
-        return new ResponseEntity<DepartmentResponse>(HttpStatus.NOT_IMPLEMENTED);
+        List<Department> departments = departmentService.departmentV1SearchPost(body);
+        ResponseHeader responseHeader = responseHeaderCreator.createResponseHeaderFromRequestHeader(body.getRequestHeader(), true);
+        DepartmentResponse departmentResponse = DepartmentResponse.builder().responseHeader(responseHeader)
+                .department(departments).build();
+        return new ResponseEntity<DepartmentResponse>(departmentResponse,HttpStatus.OK);
     }
 
 }
