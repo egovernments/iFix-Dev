a7d5bf0633f319d2b2205244e9579db7814b07b1
rahu-eGov <rajendra.kishor@egovernments.org>
2021-08-10 11:49:47 +0530

    Project search API

--- domain-services/ifix-master-data-service/src/main/java/org/egov/util/GovernmentConstants.java
+++ domain-services/ifix-master-data-service/src/main/java/org/egov/util/GovernmentConstants.java
@@ -6,6 +6,13 @@ public class GovernmentConstants {
     public static final String GOVERNMENT_NAME = "GOVERNMENT_NAME";
 
     public static final String REQUEST_PAYLOAD_MISSING = "REQUEST_PAYLOAD";
+    public static final String TENANT_ID = "TENANT_ID";
+    public static final String PROJECT_NAME = "PROJECT_NAME";
+    public static final String PROJECT_CODE = "PROJECT_CODE";
+    public static final String EAT_ID = "EAT_ID";
+    public static final String DEPARTMENT_ID = "DEPARTMENT_ID";
+    public static final String LOCATION_ID = "LOCATION_ID";
+
 
     private GovernmentConstants() {
     }
