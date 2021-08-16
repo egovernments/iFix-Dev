4a76b643a028b73738caf138a3dde64bdf5b38b1
pintu-eGov <pintu.kumar@egovernments.org>
2021-08-16 16:58:41 +0530

    IFIX-249 : - Unbundled dereference fiscal event            - disable spring web from service

---
+++ domain-services/fiscal-event-post-processor/src/main/java/org/egov/consumer/FiscalEventDereferenceConsumer.java
@@ -0,0 +1,45 @@
+package org.egov.consumer;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.egov.config.FiscalEventPostProcessorConfig;
+import org.egov.producer.Producer;
+import org.egov.service.FiscalEventUnbundleService;
+import org.egov.web.models.FiscalEventDeReferenced;
+import org.egov.web.models.FiscalEventLineItemUnbundled;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.List;
+
+@Slf4j
+@Component
+public class FiscalEventDereferenceConsumer {
+
+    @Autowired
+    private FiscalEventUnbundleService unbundleService;
+
+    @Autowired
+    private FiscalEventPostProcessorConfig processorConfig;
+
+    @Autowired
+    private Producer producer;
+
+    @Autowired
+    private ObjectMapper mapper;
+
+    @KafkaListener(topics = {"${fiscal.event.kafka.dereferenced.topic}"})
+    public void listen(final HashMap<String, Object> record, @Header(KafkaHeaders.RECEIVED_TOPIC) String topic) {
+        try {
+            FiscalEventDeReferenced fiscalEventDeReferenced = mapper.convertValue(record, FiscalEventDeReferenced.class);
+            List<FiscalEventLineItemUnbundled> fiscalEventLineItemUnbundledList  = unbundleService.unbundle(fiscalEventDeReferenced);
+            producer.push(processorConfig.getFiscalEventDruidTopic(), fiscalEventLineItemUnbundledList);
+        } catch (Exception e) {
+            log.error("Error occurred while processing the record from topic : " + topic, e);
+        }
+    }
+}
