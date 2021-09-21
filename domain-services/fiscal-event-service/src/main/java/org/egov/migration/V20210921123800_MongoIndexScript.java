5d72b0ee13e410388771e51e8b73835cc8fa0fd9
rahu-eGov <rajendra.kishor@egovernments.org>
2021-09-21 12:43:53 +0530

    fiscal event index correction

--- domain-services/fiscal-event-service/src/main/java/org/egov/migration/V20210921123800_MongoIndexScript.java
+++ domain-services/fiscal-event-service/src/main/java/org/egov/migration/V20210921123800_MongoIndexScript.java
@@ -8,35 +8,23 @@ import com.mongodb.client.MongoDatabase;
 import com.mongodb.client.model.Indexes;
 import lombok.extern.slf4j.Slf4j;
 import org.bson.Document;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.mongodb.core.MongoTemplate;
 
 
 @ChangeLog(order = "001")
 @Slf4j
-public class MongoIndexScript {
+public class V20210921123800_MongoIndexScript {
     public static final String FE_INDEX_AUTHOR = "iFIX-Fiscal-Event-Service";
 
     /**
      * @param db
      */
-    @ChangeSet(id = "V20210914184200_createBasicFiscalEventIndex", author = FE_INDEX_AUTHOR, order = "001")
-    public void createBasicFiscalEventIndex(MongoDatabase db) {
+    @ChangeSet(id = "V20210914185000_createFiscalEventIndex", author = FE_INDEX_AUTHOR, order = "001")
+    public void createFiscalEventIndex(MongoDatabase db) {
         MongoCollection<Document> mongoCollection = db.getCollection("fiscal_event");
-
         mongoCollection.createIndex(Indexes.ascending("eventType"));
         mongoCollection.createIndex(Indexes.ascending("eventTime"));
         mongoCollection.createIndex(Indexes.ascending("tenantId"));
         mongoCollection.createIndex(Indexes.ascending("referenceId"));
-    }
-
-    /**
-     * @param db
-     */
-    @ChangeSet(id = "V20210914185000_createFiscalEventIndex", author = FE_INDEX_AUTHOR, order = "002")
-    public void createFiscalEventIndex(MongoDatabase db) {
-        MongoCollection<Document> mongoCollection = db.getCollection("fiscal_event");
-
         mongoCollection.createIndex(Indexes.ascending("ingestionTime"));
         mongoCollection.createIndex(Indexes.ascending("departmentEntity"));
         mongoCollection.createIndex(Indexes.ascending("departmentEntity.hierarchyLevel"));
