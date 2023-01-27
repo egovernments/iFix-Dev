322b342f07a7f8373d8fd12a11d6fe4c4ed73408
Rushang Dhanesha <rushangdhanesha@gmail.com>
2023-01-28 02:59:53 +0530

    Fixed error in TestController

--- domain-services/ifix-es-pipeline/src/main/java/org/egov/ifixespipeline/controller/TestController.java
+++ domain-services/ifix-es-pipeline/src/main/java/org/egov/ifixespipeline/controller/TestController.java
@@ -5,7 +5,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.egov.ifixespipeline.models.FiscalClientInput;
 import org.egov.ifixespipeline.models.FiscalEvent;
-import org.egov.ifixespipeline.models.FiscalEventRequest;
+import org.egov.ifixespipeline.models.FiscalEventBulkRequest;
 import org.egov.ifixespipeline.service.FiscalDataEnrichmentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -29,15 +29,17 @@ public class TestController {
     ObjectMapper objectMapper;
 
     @RequestMapping(value="/v1/_test", method = RequestMethod.POST)
-    public ResponseEntity<FiscalEvent> testNewAuditFlow(@RequestBody @Valid FiscalClientInput input) throws JsonProcessingException {
+    public ResponseEntity<FiscalEventBulkRequest> testNewAuditFlow(@RequestBody @Valid FiscalClientInput input) throws JsonProcessingException {
         log.info("Received request: " + input.toString());
 //        List<Integer> responseHash = ingestService.ingestData(ingestRequest);
 //        //log.info("############ Completed before pushing data");
 //        ResponseInfo responseInfo = responseInfoFactory.createResponseInfoFromRequestInfo(ingestRequest.getRequestInfo(), true);
 //        IngestResponse response = IngestResponse.builder().responseInfo(responseInfo).responseHash(responseHash).build();
-        FiscalEventRequest fiscalEventRequest = objectMapper.readValue(input.getJson(), FiscalEventRequest.class);
-        fiscalDataEnrichmentService.enrichFiscalData(fiscalEventRequest);
-        //fiscalDataEnrichmentService.enrichComputedFields(fiscalEventRequest);
-        return new ResponseEntity<>(fiscalEventRequest.getFiscalEvent(), HttpStatus.OK);
+        FiscalEventBulkRequest fiscalEventBulkRequest = objectMapper.readValue(input.getJson(), FiscalEventBulkRequest.class);
+        for(FiscalEvent fiscalEvent : fiscalEventBulkRequest.getFiscalEvent()) {
+            fiscalDataEnrichmentService.enrichFiscalData(fiscalEvent);
+//            fiscalDataEnrichmentService.enrichComputedFields(fiscalEvent);
+        }
+        return new ResponseEntity<>(fiscalEventBulkRequest, HttpStatus.OK);
     }
 }
