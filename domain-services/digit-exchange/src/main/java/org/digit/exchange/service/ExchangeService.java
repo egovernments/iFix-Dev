5183341f84afcdaf1ee83ff7527105c1a09d7f5a
shubhang-egov <shubhang.dhanesha@egovernments.org>
2024-03-12 14:37:36 +0530

    adding Exception handling in digit exchange

--- domain-services/digit-exchange/src/main/java/org/digit/exchange/service/ExchangeService.java
+++ domain-services/digit-exchange/src/main/java/org/digit/exchange/service/ExchangeService.java
@@ -3,6 +3,7 @@ package org.digit.exchange.service;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import lombok.extern.slf4j.Slf4j;
 import org.digit.exchange.config.AppConfig;
 import org.digit.exchange.constants.ExchangeType;
@@ -11,11 +12,10 @@ import org.digit.exchange.model.JsonMessage;
 import org.digit.exchange.model.RequestMessage;
 import org.digit.exchange.model.RequestMessageWrapper;
 import org.digit.exchange.repository.ServiceRequestRepository;
+import org.egov.tracer.model.CustomException;
 import org.springframework.stereotype.Service;
 
 import javax.servlet.ServletContext;
-import java.net.URI;
-import java.net.URISyntaxException;
 
 @Service
 @Slf4j
@@ -25,7 +25,7 @@ public class ExchangeService {
     private final ObjectMapper mapper;
     private final ServiceRequestRepository restRepo;
     private final SecurityService securityService;
-    private ServletContext servletContext;
+    private final ServletContext servletContext;
 
 
     public ExchangeService(ExchangeProducer producer, AppConfig config, ObjectMapper mapper, ServiceRequestRepository restRepo, SecurityService securityService,  ServletContext servletContext) {
@@ -61,17 +61,22 @@ public class ExchangeService {
             try {
                 jsonNode = mapper.readTree(requestMessage.getMessage());
             } catch (JsonProcessingException e) {
-                throw new RuntimeException(e);
+                throw new CustomException("JSON_PROCESSING_EXCEPTION", e.getMessage());
             }
 
             log.info("Converted message to Json Node");
-            JsonMessage jsonMessage = JsonMessage.builder().signature(requestMessage.getSignature()).header(requestMessage.getHeader()).jsonNode(jsonNode).build();
+            JsonMessage jsonMessage = JsonMessage.builder().signature(requestMessage.getSignature())
+                    .header(requestMessage.getHeader()).jsonNode(jsonNode).build();
             try {
                 restRepo.fetchResult(url, jsonMessage);
                 log.info("Posted request to : {}", url);
-            } catch (Exception e) {
-                log.error("Exception while calling the API : {}", e.getMessage());
-//                handleErrorForSameDomain(requestMessageWrapper, isReply);
+            } catch (CustomException exception) {
+                log.error("Exception while calling the API : {}", exception.getMessage());
+                handleErrorForSameDomain(requestMessageWrapper, isReply, exception);
+            } catch (Exception exception) {
+                log.error("Exception while calling the API : {}", exception.getMessage());
+                handleErrorForSameDomain(requestMessageWrapper, isReply, new CustomException("EXTERANL_SERVICE_ERROR",
+                        exception.getMessage()));
             }
         } else {
             // If sending the request from this server to another server then sign the message
@@ -81,9 +86,12 @@ public class ExchangeService {
             try {
                 restRepo.fetchResult(url, requestMessage);
                 log.info("Posted request to : {}", url);
-            } catch (Exception e) {
-                log.error("Exception while calling the API : {}", e.getMessage());
-//                handleErrorForDifferentDomain(requestMessageWrapper, isReply);
+            } catch (CustomException exception) {
+                log.error("Exception while calling the API : {}", exception.getMessage());
+                handleErrorForDifferentDomain(requestMessageWrapper, isReply, exception);
+            } catch (Exception exception) {
+                log.error("Exception while calling the API : {}", exception.getMessage());
+                handleErrorForDifferentDomain(requestMessageWrapper, isReply, new CustomException("EXTERANL_SERVICE_ERROR", exception.getMessage()));
             }
         }
 
@@ -96,7 +104,8 @@ public class ExchangeService {
             RequestMessage requestMessage = requestMessageWrapper.getRequestMessage();
             jsonNode = mapper.readTree(requestMessageWrapper.getRequestMessage().getMessage());
             log.info("Converted message to Json Node");
-            JsonMessage jsonMessage = JsonMessage.builder().signature(requestMessage.getSignature()).header(requestMessage.getHeader()).jsonNode(jsonNode).build();
+            JsonMessage jsonMessage = JsonMessage.builder().signature(requestMessage.getSignature())
+                    .header(requestMessage.getHeader()).jsonNode(jsonNode).build();
             producer.push( config.getExchangeEventLogTopic(), jsonMessage);
             log.info("Pushed message to kafka topic : " + config.getExchangeEventLogTopic());
         }catch (Exception e) {
@@ -104,18 +113,15 @@ public class ExchangeService {
         }
     }
 
-    private void handleErrorForSameDomain(RequestMessageWrapper requestMessageWrapper, Boolean isReply) {
+    private void handleErrorForSameDomain(RequestMessageWrapper requestMessageWrapper, Boolean isReply, CustomException exception) {
         if (Boolean.TRUE.equals(isReply)) {
             log.info("Pushing error to error topic for same domain");
             producer.push(config.getErrorTopic(), requestMessageWrapper);
         } else {
             log.info("Swapping receiver and sender domain");
-            String receiverDomain = requestMessageWrapper.getRequestMessage().getHeader().getSenderId();
-            String senderDomain = requestMessageWrapper.getRequestMessage().getHeader().getReceiverId();
-            requestMessageWrapper.getRequestMessage().getHeader().setReceiverId(receiverDomain);
-            requestMessageWrapper.getRequestMessage().getHeader().setSenderId(senderDomain);
-            requestMessageWrapper.setType(ExchangeType.fromValue("on-" + requestMessageWrapper.getType().toString()));
+            updateHeaderDomains(requestMessageWrapper);
             String newUrl = getReceiverEndPoint(requestMessageWrapper.getRequestMessage(), true);
+            convertMessageAndSetStatus(requestMessageWrapper.getRequestMessage(), exception);
             try {
                 restRepo.fetchResult(newUrl, requestMessageWrapper.getRequestMessage());
             } catch (Exception ex) {
@@ -126,17 +132,14 @@ public class ExchangeService {
         }
     }
 
-    private void handleErrorForDifferentDomain(RequestMessageWrapper requestMessageWrapper, Boolean isReply) {
+    private void handleErrorForDifferentDomain(RequestMessageWrapper requestMessageWrapper, Boolean isReply, CustomException exception) {
         if (Boolean.TRUE.equals(isReply)) {
             producer.push(config.getErrorTopic(), requestMessageWrapper);
         } else {
             log.info("Swapping receiver and sender domain");
-            String receiverDomain = requestMessageWrapper.getRequestMessage().getHeader().getSenderId();
-            String senderDomain = requestMessageWrapper.getRequestMessage().getHeader().getReceiverId();
-            requestMessageWrapper.getRequestMessage().getHeader().setReceiverId(receiverDomain);
-            requestMessageWrapper.getRequestMessage().getHeader().setSenderId(senderDomain);
-            requestMessageWrapper.setType(ExchangeType.fromValue("on-" + requestMessageWrapper.getType().toString()));
+            updateHeaderDomains(requestMessageWrapper);
             String newUrl = getReceiverEndPoint(requestMessageWrapper.getRequestMessage(), true);
+            convertMessageAndSetStatus(requestMessageWrapper.getRequestMessage(), exception);
             log.info("Converting message to Json Node");
             JsonNode newNode;
             try {
@@ -145,7 +148,10 @@ public class ExchangeService {
                 throw new RuntimeException(ex);
             }
 
-            JsonMessage newJsonMessage = JsonMessage.builder().signature(requestMessageWrapper.getRequestMessage().getSignature()).header(requestMessageWrapper.getRequestMessage().getHeader()).jsonNode(newNode).build();
+            JsonMessage newJsonMessage = JsonMessage.builder()
+                    .signature(requestMessageWrapper.getRequestMessage().getSignature())
+                    .header(requestMessageWrapper.getRequestMessage().getHeader())
+                    .jsonNode(newNode).build();
             try {
                 restRepo.fetchResult(newUrl, newJsonMessage);
             } catch (Exception ex) {
@@ -155,6 +161,13 @@ public class ExchangeService {
         }
     }
 
+    void updateHeaderDomains(RequestMessageWrapper requestMessageWrapper) {
+        String receiverDomain = requestMessageWrapper.getRequestMessage().getHeader().getSenderId();
+        String senderDomain = requestMessageWrapper.getRequestMessage().getHeader().getReceiverId();
+        requestMessageWrapper.getRequestMessage().getHeader().setReceiverId(receiverDomain);
+        requestMessageWrapper.getRequestMessage().getHeader().setSenderId(senderDomain);
+        requestMessageWrapper.setType(ExchangeType.fromValue("on-" + requestMessageWrapper.getType().toString()));
+    }
 
     String getReceiverEndPoint(RequestMessage message, Boolean isReply){
         if(securityService.isSameDomain(message.getHeader().getReceiverId(), config.getDomain())){
@@ -169,6 +182,37 @@ public class ExchangeService {
             String receiverDomain = securityService.extractHostUrlFromURL(message.getHeader().getReceiverId());
             String contextPath = servletContext.getContextPath();
             return receiverDomain + contextPath +"/v1/exchange/" + message.getHeader().getMessageType().toString();
+        }
+    }
+
+    void convertMessageAndSetStatus(RequestMessage requestMessage, CustomException exception) {
+        ObjectNode objectNode;
+        try {
+            objectNode = mapper.readValue(requestMessage.getMessage(), ObjectNode.class);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+        setErrorStatuses(objectNode, exception);
+
+        try {
+            requestMessage.setMessage(mapper.writeValueAsString(objectNode));
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    void setErrorStatuses(ObjectNode objectNode, CustomException exception) {
+
+        if (objectNode.has("status"))
+            objectNode.remove("status");
+        ObjectNode statusNode = mapper.createObjectNode();
+        statusNode.put("status_code", "ERROR");
+        statusNode.put("status_message", exception.getCode() + " " + exception.getMessage());
+        objectNode.set("status", statusNode);
+        if (objectNode.has("children") && objectNode.get("children").isArray()) {
+            for (JsonNode child : objectNode.get("children")) {
+                setErrorStatuses((ObjectNode) child, exception);
+            }
         }
     }
 }
